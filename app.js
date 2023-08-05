aef20499a91262865d17b9c9bd5f0fcb4ee7d8e7
AJAY NAIK RAMAVATH <ajay@AJAYs-MacBook-Air.local>
2023-08-05 13:42:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+})
+
+
+
+app.post("/",function(req,res){
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address : email,
+        status : "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname
+        }
+
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/709cf863b9"
+
+  const options = {
+    method:"POST",
+    auth: "ajay:4b896f97ccad03af21061d5e49d6ac82-us8"
+  }
+
+  const request = https.request(url, options , function(response){
+
+    if(response.statusCode === 200) {
+      res.sendFile(__dirname+"/success.html");
+    }else {
+      res.sendFile(__dirname+"/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+
+})
+
+
+//apiKey--4b896f97ccad03af21061d5e49d6ac82-us8
+//id--709cf863b9
